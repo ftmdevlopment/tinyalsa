5e4375df448122eada69b0cbc0efe7dc38c82cc6
Siwei Xu <xusiwei1236@163.com>
2016-12-13 14:01:13 +0800

    fix ndk-build fail

--- Android.mk
+++ Android.mk
@@ -1,26 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_C_INCLUDES:= $(C_ROOT)/tinyalsa/include
 LOCAL_SRC_FILES:= mixer.c pcm.c
 LOCAL_MODULE := libtinyalsa
 LOCAL_SHARED_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 
-include $(BUILD_SHARED_LIBRARY)
-
-ifeq ($(HOST_OS), linux)
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= mixer.c pcm.c
-LOCAL_MODULE := libtinyalsa
-LOCAL_STATIC_LIBRARIES:= libcutils libutils
-include $(BUILD_HOST_STATIC_LIBRARY)
-endif
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_C_INCLUDES:= $(C_ROOT)/tinyalsa/include
 LOCAL_SRC_FILES:= tinyplay.c
 LOCAL_MODULE := tinyplay
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
@@ -28,19 +19,8 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
 
-ifeq ($(HOST_OS), linux)
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinyplay.c
-LOCAL_MODULE := tinyplay
-LOCAL_STATIC_LIBRARIES:= libcutils libutils libtinyalsa
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_HOST_EXECUTABLE)
-endif
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_C_INCLUDES:= $(C_ROOT)/tinyalsa/include
 LOCAL_SRC_FILES:= tinycap.c
 LOCAL_MODULE := tinycap
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
@@ -49,7 +29,7 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_C_INCLUDES:= $(C_ROOT)/tinyalsa/include
 LOCAL_SRC_FILES:= tinymix.c
 LOCAL_MODULE := tinymix
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
@@ -58,7 +38,7 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_C_INCLUDES:= $(C_ROOT)/tinyalsa/include
 LOCAL_SRC_FILES:= tinypcminfo.c
 LOCAL_MODULE := tinypcminfo
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
